96fc048347174279811baf38938cab2bf9067cfb
Linnea <lg223fg@studrent.lnu.se>
2025-12-15 14:57:05 +0100

    Add character count display

--- contact.js
+++ contact.js
@@ -8,16 +8,26 @@ const successMessage = document.getElementById("successMessage");
 const emailErrorMessage = document.getElementById("emailErrorMessage")
 
 form.addEventListener("submit", function (event) {
-    event.preventDefault();
+    event.preventDefault(); // prevent browsers default form submission
 
-    if (!email.value.includes("@")) {
-        emailErrorMessage.textContent = "Enter a valid Email-adress"
-        emailErrorMessage.style.color = "red"
+    if (!email.value.includes("@")) { //check if the field contains an "@"
+        emailErrorMessage.textContent = "Enter a valid Email-adress" //if its invalid, error message show
+        emailErrorMessage.style.color = "red" //color of the text
+
     } else {
-        form.submit()
-        successMessage.textContent = "Thanks for your message"
-        successMessage.style.color = "green"
-        emailErrorMessage.textContent = ""
-        form.reset()
+        form.submit() //if email is valid, its submitting the form
+        successMessage.textContent = "Thanks for your message" //success message
+        successMessage.style.color = "green" //color of the text
+        emailErrorMessage.textContent = "" //clear the error message
+        form.reset() //reset all inputs after submitting
     }
 });
+
+const textarea = document.getElementById("message");
+const charCount = document.getElementById("charCount");
+
+textarea.addEventListener("keyup", function(){
+    const text = textarea.value; //get the text from texarea
+    const count = text.length; //count number of signs
+    charCount.textContent = "Character count: " + count;
+})
