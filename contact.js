c0a30803c52a53eb843bf0d660a5453257c3c04d
Linnea <lg223fg@studrent.lnu.se>
2025-12-15 14:20:09 +0100

    Implement form submission handling with email validation and success/error messages

--- contact.js
+++ contact.js
@@ -5,5 +5,19 @@ const email = document.getElementById("email");
 const subject = document.getElementById("subject");
 const message = document.getElementById("message");
 const successMessage = document.getElementById("successMessage");
-const errorMessage = document.getElementById("errorMessage")
+const emailErrorMessage = document.getElementById("emailErrorMessage")
 
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    if (!email.value.includes("@")) {
+        emailErrorMessage.textContent = "Enter a valid Email-adress"
+        emailErrorMessage.style.color = "red"
+    } else {
+        form.submit()
+        successMessage.textContent = "Thanks for your message"
+        successMessage.style.color = "green"
+        emailErrorMessage.textContent = ""
+        form.reset()
+    }
+});
