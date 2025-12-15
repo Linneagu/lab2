6a0fd5043faccee70e57bbc00e7ae9185d6af5a6
Linnea <lg223fg@studrent.lnu.se>
2025-12-15 12:36:22 +0100

    Added DOM selection for all element in the form

---
+++ contact.js
@@ -0,0 +1,9 @@
+const form = document.getElementById("contact");
+const firstName = document.getElementById("firstName");
+const lastName = document.getElementById("lastName");
+const email = document.getElementById("email");
+const subject = document.getElementById("subject");
+const message = document.getElementById("message");
+const successMessage = document.getElementById("successMessage");
+const errorMessage = document.getElementById("errorMessage")
+
