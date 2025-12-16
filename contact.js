cae640e30253e339485d581fea24c43cfccbeae1
Linnea <lg223fg@studrent.lnu.se>
2025-12-16 09:36:49 +0100

    changed so basecolor is black on character counter

--- contact.js
+++ contact.js
@@ -14,11 +14,9 @@ form.addEventListener("submit", function (event) { //add event listener when sub
     if (!email.value.includes("@")) { //check if the field contains an "@"
         emailErrorMessage.textContent = "Enter a valid Email-adress" //if its invalid, error message show
         emailErrorMessage.style.color = "red" //color of the text
-
     } else {
         form.submit() //if email is valid, its submitting the form
-        successMessage.textContent = "Thanks for your message" //success message
-        successMessage.style.color = "green" //color of the text
+        successMessage.textContent = `Thanks for your message ${firstName.value}!` //success message
         emailErrorMessage.textContent = "" //clear the error message
         form.reset() //reset all inputs after submitting
     }
@@ -28,17 +26,19 @@ form.addEventListener("submit", function (event) { //add event listener when sub
 const textarea = document.getElementById("message");
 const charCount = document.getElementById("charCount");
 
-textarea.addEventListener("keyup", function(){ //add event listener every time you releases a key
+textarea.addEventListener("keyup", function () { //add event listener every time you releases a key
     const text = textarea.value; //get the text from texarea
     const count = text.length; //count the characters
 
-    charCount.textContent = count + `/20 characters `; //update the text content of the character conunter and shows how many out of 20
+    charCount.textContent = count + `/20 characters`; //update the text content of the character conunter and shows how many out of 20
 
-    charCount.classList.remove("red", "green") //
-
-    if(count <20) { //if character count is below 20, number shows in red
+    if (count < 20 && count > 0) { //if character count is below 20, number shows in red
+        charCount.classList.remove("base", "green")
         charCount.classList.add("red");
-    } else { //outherwise show in green
+    } else if (count >= 20) {
+        charCount.classList.remove("base", "red")
         charCount.classList.add("green");
+    } else {
+        charCount.classList.add("base")
     }
 });
