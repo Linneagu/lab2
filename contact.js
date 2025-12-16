73b0753390c6d43506b7112e71093e405bf5323f
Linnea <lg223fg@studrent.lnu.se>
2025-12-16 13:01:19 +0100

    Separate elements & add comments to group them, add function to validate email input, update submit event listener to show success message and reset

--- contact.js
+++ contact.js
@@ -1,44 +1,63 @@
-const form = document.getElementById("contact");
-const firstName = document.getElementById("firstName");
-const lastName = document.getElementById("lastName");
-const email = document.getElementById("email");
-const subject = document.getElementById("subject");
-const message = document.getElementById("message");
+const formField = document.getElementById("contact");
+
+// HTML input, select & textarea elements
+const firstNameInput = document.getElementById("firstName");
+const lastNameInput = document.getElementById("lastName");
+const emailInput = document.getElementById("email");
+const subjectSelect = document.getElementById("subject");
+const messageTextarea = document.getElementById("message");
+
+// HTML p elements for showing errors
+const firstNameError = document.getElementById("firstNameError");
+const lastNameError = document.getElementById("lastNameError");
+const emailError = document.getElementById("emailErrorMessage")
+const messageError = document.getElementById("messageError")
 const successMessage = document.getElementById("successMessage");
-const emailErrorMessage = document.getElementById("emailErrorMessage")
 
-//Error message for email. 
-form.addEventListener("submit", function (event) { //add event listener when submit the form
-    event.preventDefault(); // prevent browsers default form submission
-
-    if (!email.value.includes("@")) { //check if the field contains an "@"
-        emailErrorMessage.textContent = "Enter a valid Email-adress" //if its invalid, error message show
-        emailErrorMessage.style.color = "red" //color of the text
-    } else {
-        form.submit() //if email is valid, its submitting the form
-        successMessage.textContent = `Thanks for your message ${firstName.value}!` //success message
-        emailErrorMessage.textContent = "" //clear the error message
-        form.reset() //reset all inputs after submitting
-    }
-});
-
-//Character counter
-const textarea = document.getElementById("message");
+// HTML element for showing character count of textarea
 const charCount = document.getElementById("charCount");
 
-textarea.addEventListener("keyup", function () { //add event listener every time you releases a key
-    const text = textarea.value; //get the text from texarea
-    const count = text.length; //count the characters
+function validateEmail(inputElement, errorElement) {
+    const value = inputElement.value.trim();
 
-    charCount.textContent = count + `/20 characters`; //update the text content of the character conunter and shows how many out of 20
+    if (!value.includes("@") || !value.includes(".")) {
+        showError("Enter a valid email", errorElement);
+        return false;
+    }
 
-    if (count < 20 && count > 0) { //if character count is below 20, number shows in red
-        charCount.classList.remove("base", "green")
-        charCount.classList.add("red");
-    } else if (count >= 20) {
-        charCount.classList.remove("base", "red")
-        charCount.classList.add("green");
+    clearError(errorElement);
+    return true;
+}
+
+function clearForm() {
+
+}
+
+formField.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const emailValid = validateEmail(emailInput, emailError);
+
+    successMessage.textContent = `Thank you, ${firstNameInput.value}! I will contact you soon.`;
+    successMessage.style.color = "green";
+
+    setTimeout(() => {
+        successMessage.textContent = "";
+    }, 3000);
+
+    formField.reset();
+
+    charCount.textContent = "0/20 characters";
+    charCount.style.color = "black";
+})
+
+messageTextarea.addEventListener("keyup", function () {
+    const count = messageTextarea.value.length;
+    charCount.textContent = `${count}/20 characters`;
+
+    if (count < 20) {
+        charCount.style.color = "red";
     } else {
-        charCount.classList.add("base")
+        charCount.style.color = "green"
     }
 });
