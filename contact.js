44896e8f1011405ff908c7832705e92cef15ba01
Linnea <lg223fg@studrent.lnu.se>
2025-12-17 11:35:02 +0100

    fixed success message

--- contact.js
+++ contact.js
@@ -11,7 +11,7 @@ const messageTextarea = document.getElementById("message");
 const firstNameError = document.getElementById("firstNameError");
 const lastNameError = document.getElementById("lastNameError");
 const emailError = document.getElementById("emailErrorMessage")
-const subjectError = document.getElementById("subjectError");
+const subjectError= document.getElementById("subjectError");
 const messageError = document.getElementById("messageError");
 const successMessage = document.getElementById("successMessage");
 
@@ -20,24 +20,25 @@ const charCount = document.getElementById("charCount");
 
 const clearButton = document.getElementById("clear");
 
-// Function for showError and clearError
+//Function for showError and clearError
 function showError(message, inputElement, errorElement) {
-    errorElement.classList.remove("hidden");
     errorElement.textContent = message;
+    errorElement.classList.remove("hidden");
     errorElement.style.color = "red";
     inputElement.classList.add("error");
+    inputElement.classList.remove("valid")
 }
 
 function clearError(inputElement, errorElement) {
     errorElement.textContent = "";
-    inputElement.classList.remove("error");
     errorElement.classList.add("hidden");
+    inputElement.classList.remove("error");
+    inputElement.classList.add("valid")
 }
 
-// Function for validation
-// Name
+//Function for validation
 function validateName(inputElement, errorElement) {
-    const value = inputElement.value.trim();
+    const value = inputElement.value.trim()
     const regex = /^[A-Za-z]+$/;
 
     if (!regex.test(value)) {
@@ -46,19 +47,9 @@ function validateName(inputElement, errorElement) {
     }
 
     clearError(inputElement, errorElement);
-    inputElement.classList.add("valid");
     return true;
 }
 
-firstNameInput.addEventListener("input", function (){
-    validateName(firstNameInput, firstNameError);
-})
-
-lastNameInput.addEventListener("input", function (){
-    validateName(lastNameInput, lastNameError);
-})
-
-// Email
 function validateEmail(inputElement, errorElement) {
     const value = inputElement.value.trim();
 
@@ -68,32 +59,20 @@ function validateEmail(inputElement, errorElement) {
     }
 
     clearError(inputElement, errorElement);
-    inputElement.classList.add("valid");
     return true;
 }
 
-emailInput.addEventListener("input", function () {
-    validateEmail(emailInput, emailError);
-})
-
-// Subject
 function validateSubject(inputElement, errorElement) {
-    console.log("select value", inputElement.value)
     if (inputElement.value === "") {
         showError("Please choose a subject", inputElement, errorElement);
         return false;
     }
 
     clearError(inputElement, errorElement);
-    inputElement.classList.add("valid");
+    inputElement.classList.add("valid")
     return true;
 }
 
-subjectSelect.addEventListener("input", function () {
-    validateSubject(subjectSelect, subjectError);
-})
-
-// Message
 function validateMessage(inputElement, errorElement) {
     const value = inputElement.value.trim();
 
@@ -106,25 +85,6 @@ function validateMessage(inputElement, errorElement) {
     return true;
 }
 
-messageTextarea.addEventListener("input", function () {
-validateMessage(messageTextarea, messageError);
-})
-
-// Char count
-messageTextarea.addEventListener("keyup", function () {
-    const count = messageTextarea.value.length;
-    charCount.textContent = `${count}/20 characters`;
-
-    if (count < 20) {
-        charCount.style.color = "red";
-        messageTextarea.classList.remove("valid")
-    } else {
-        charCount.style.color = "green"
-        messageTextarea.classList.add("valid")
-    }
-});
-
-// Clear form
 function clearForm() {
     formField.reset();
 
@@ -139,17 +99,12 @@ function clearForm() {
     emailInput.classList.remove("error", "valid");
     subjectSelect.classList.remove("error", "valid");
     messageTextarea.classList.remove("error", "valid");
-
+    
 
     charCount.textContent = "0/20 characters";
     charCount.style.color = "black";
-
-    setTimeout(() => {
-        successMessage.textContent = "";
-    }, 3000);
 }
 
-// Submit
 formField.addEventListener("submit", function (event) {
     event.preventDefault();
 
@@ -163,10 +118,25 @@ formField.addEventListener("submit", function (event) {
         return;
     }
 
+    successMessage.classList.remove("hidden");
     successMessage.textContent = `Thank you, ${firstNameInput.value}! I will contact you soon.`;
     successMessage.style.color = "green";
 
-    clearForm();
+    setTimeout(() => {
+        successMessage.textContent = "";
+        clearForm();
+    }, 3000);
 })
+
+messageTextarea.addEventListener("keyup", function () {
+    const count = messageTextarea.value.length;
+    charCount.textContent = `${count}/20 characters`;
+
+    if (count < 20) {
+        charCount.style.color = "red";
+    } else {
+        charCount.style.color = "green"
+    }
+});
 
 clearButton.addEventListener("click", clearForm);
